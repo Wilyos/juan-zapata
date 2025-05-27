08be1dcfa21815df32ea7ce1608fa46e86fad2f6
wilyos <wilyos99@gmail.com>
2025-05-27 16:14:12 -0500

    Correcciones

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ sobreMi.onclick = () => {
 const vcardData = `
 BEGIN:VCARD
 VERSION:3.0
-FN:Carlos Sierra Pattin
-ORG:Sierra Pattin
-TEL;TYPE=CELL:+57 3164803467
-EMAIL:gerencia@sierraseguros.com
+FN:Juan Fernando Zapata
+ORG:Sierra Pattin Seguros
+TEL;TYPE=CELL:+57 3217151286
+EMAIL:juanzapata@sierraseguros.com 
 END:VCARD
   `.trim();
 
@@ -39,3 +39,20 @@ END:VCARD
 
   const downloadLink = document.getElementById('downloadContact');
   downloadLink.href = url;
+
+ // formulario
+  document.getElementById('contactForm').addEventListener('submit', async function(e) {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(this));
+  const res = await fetch('https://sierra-seguros-back.onrender.com/send', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+  if (res.ok) {
+    alert('Mensaje enviado correctamente');
+    this.reset();
+  } else {
+    alert('Error enviando mensaje');
+  }
+});
